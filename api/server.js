528f4d431bd4297af95ae2d4da2f3c17b251f1dc
sam-malanchuk <malanchuksam@gmail.com>
2019-09-10 21:10:20 -0400

    installed dotenv and created the file to store the environment variable

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+require('dotenv').config();
 
 const UsersRouter = require('../users/usersRouter.js');
 const AuthRouter = require('../auth/authRouter.js');
